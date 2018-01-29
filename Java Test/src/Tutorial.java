239d10bd07564081054a128a48431a2d6e662c80
downs19 <downs.jamie@gmail.com>
2018-01-29 07:52:30 -0500

    java

---
+++ Java Test/src/Tutorial.java
@@ -0,0 +1,11 @@
+class Tutorial {
+    public static main(String[] args){
+        
+        
+        // comment
+        //int a = 25;
+        //System.out.println(a);
+        
+        
+    }
+}
---
+++ javaTest.java
@@ -0,0 +1,20 @@
+class javaTest {
+	public static void main(String[] args){
+		//System.out.println("Hello World!");
+		// int a = 25;
+		// int b = 30;
+		// int c = a + b + 5;
+		// System.out.println(c);
+		// System.out.println(b);
+		// System.out.println(a);
+		byte b; // 8 bits
+		short s; // 16 bits
+		int i; // 32 bits
+		long l; // 64 bits
+
+		float f = 3.14f; // 32 bits
+		double d = 3.14d; // 64. bits
+
+		System.out.println(f);
+	}
+}
